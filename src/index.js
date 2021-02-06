dae663c24ef19cd64f36aa3393336b583a01e3da
shashankm@exzeo.com <shashankm@exzeo.com>
2021-02-06 10:01:55 +0530

    Add and remove values

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './common/common.css';
+import PhoneDirectory from './PhoneDirectory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<PhoneDirectory />, document.getElementById('root'));
 registerServiceWorker();
